ff9e63d3d32a80ba76451af2439d50e9aa5aa35a
Dhenix <116231111+Dhenix@users.noreply.github.com>
2022-10-20 01:19:02 +0000

    Aprendi algo novo dalle2!

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ idade = 21
 print(nome)
 
 #Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade
+#print("Meu nome é "+nome)
 print("Minha idade é "+str(idade)+" anos")
+print(f"Minha idade é {idade} anos")
+print("Minha idade é {} anos".format(idade))
+#Usar "\n" para pular uma linha no console.
+#Quando quiser exibir "Mmeu nome é ... e tenho ... anos..." trocando pelas variáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
 
 
 
