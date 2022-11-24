c424842f9b9f7ca4ab5d12810b5d1eae00c40ada
Dhenix <116231111+Dhenix@users.noreply.github.com>
2022-11-24 01:26:51 +0000

    APRENENDO A FACILITAR AS CONTAS DO MEU CURSO DE ARQUITETURA...

---
+++ CALCULAR VOLUME.py
@@ -0,0 +1,7 @@
+print('this will help you find the volume of a rectangle')
+a=int(input('Enter your first number here '))
+b=int(input('Enter your second number here '))
+c=int(input('Ente r your third number here '))
+print('the volume your rectangle is',a*b*c)
+
+#COMO FAÇO ARQUITETURA ISSO VAI AJUDAR BASTANTE
