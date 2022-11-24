e1635714698e9ec3ed4618c325e6127b894be4e2
Dhenix <116231111+Dhenix@users.noreply.github.com>
2022-11-24 01:18:42 +0000

    APRENDI A CALCULAR A MEDIA DO COEGIO DO MEU IRMÃO COM 4 NOTAS DIFERENTES, DOIS TRABALHOS E DUAS PROVAS...

---
+++ CALCULANDO MEDIA DO COLEGIO DE MEU IRMÃO.py
@@ -0,0 +1,14 @@
+# Escreva o seu código aqui :-)
+p1 = float(input("Digite a nota da prova 1: "))
+
+p2 = float(input("Digite a nota da prova 2: "))
+
+t1 = float(input("Digite a nota da trabalho 1: "))
+
+t2 = float(input("Digite a nota da trabalho 2: "))
+
+m = (p1 + p2 + t1 + t2) / 4
+
+print("Média: ", m)
+
+#Calculando media do colegio de meu irmão kkk
