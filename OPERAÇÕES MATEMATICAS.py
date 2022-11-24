e9c9fd07c5faee8a52ee6f3bb4d3c7739855683e
Dhenix <116231111+Dhenix@users.noreply.github.com>
2022-11-24 01:23:33 +0000

    TENTANDO USAR ALGUMAS OPERAÇÕES MATEMATICAS, ESPERO MELHORAR... CONTINUAREI TENTANO...

---
+++ OPERAÇÕES MATEMATICAS.py
@@ -0,0 +1,26 @@
+#estudos operadores matematocos 
+#soma+
+#subtraçao-
+#maultiplicaçao* 
+#divisao/
+#potenciaçao **
+#resto da divisao % nada a ver com porcentagem matematica 
+
+numero1=5
+numero2=7
+resultado_soma=numero1+numero2
+print("o resultado da soma e",resultado_soma)
+
+resultado_subtracao=numero1-numero2
+print("o resultado da subtraçao e",resultado_subtracao)
+
+resultado_divisao=numero1/numero2
+print("o resultado da divisao e",resultado_divisao)
+
+resultado_multiplicacao=numero1*numero2
+print("resultado multiplicacao e",resultado_multiplicacao)
+
+resultado_potenciacao=numero1**numero2 
+print("potenciaçao')
+
+#PRECISO TREINAR MAIS......
